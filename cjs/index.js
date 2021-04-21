99f2101e692f78ffd34c502a883ecf0373fc756c
loic.flagollet <loic.flagollet@launchmetrics.com>
2021-04-21 15:05:00 +0200

    Push cjs folder to github

---
+++ cjs/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var Img = require('./Img.js');
+var useImage = require('./useImage-cea931dd.js');
+require('@babel/runtime/helpers/objectWithoutPropertiesLoose');
+require('react');
+require('@babel/runtime/helpers/defineProperty');
+
+
+
+exports.Img = Img;
+exports.useImage = useImage.useImage;
+//# sourceMappingURL=index.js.map
